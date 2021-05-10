34c45c5e0c1305ae158c40c688cf860e216f33ac
Omer Noor <omernoor16@gmail.com>
2021-05-10 02:32:20 -0400

    Fix Search

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,12 +2,14 @@ var imgModel = require('../models/image');
 
 //Searches within the database for images by complete tag match or partial match in title (using regex)
 const searchImages = (req, res,) => {
+    let user = req.query.user
+    if (req.query.user==="/"){user=undefined}
     imgModel.find({$or:[{tags:req.query.search},{title:{ "$regex": req.query.search, "$options": "i" }}]}, (err, items) => {
         if (err) {
             console.log(err);
             res.status(500).send('An error occurred', err);
         } else {
-            res.render('index', {items: items});
+            res.render('index', {items: items, user:user});
         }
     });
 }
